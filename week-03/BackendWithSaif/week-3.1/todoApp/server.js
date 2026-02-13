8fc134cee3d8fb2b07d2877c98beade26389043a
uzma-khan98 <uzma.uk.1998@gmail.com>
2026-02-14 01:01:41 +0500

    commented zod validation

--- week-03/BackendWithSaif/week-3.1/todoApp/server.js
+++ week-03/BackendWithSaif/week-3.1/todoApp/server.js
@@ -1,16 +1,14 @@
 import express from "express";
-import zod from "zod";
+// import zod from "zod";
 
 const app = express();
 //* middlewares
 app.use(express.json());
 
-
 //* schema with zod
-const todosSchema = zod.object({
-  text: zod.string().min(1,"Todo text cannot be empty"),
-  
-})
+// const todosSchema = zod.object({
+//   text: zod.string().min(1, "Todo text cannot be empty"),
+// });
 //* todos-data  (there's an object in an array)
 let ALL_TODOS = [
   {
@@ -18,24 +16,39 @@ let ALL_TODOS = [
     completed: false,
     id: 1,
   },
+  {
+    text: "Doing breakfast",
+    completed: false,
+    id: 2,
+  },
 ];
+
+// * Error-handling middleware
+function errorHandlingMiddleware(err, req, res, next) {
+  if (err) {
+    res.status(500).json({
+      msg: "Something went wrong!",
+    });
+  }
+  next()
+}
 
 //* Creating API routes for CRUD
 //* to post(create) a todo
 app.post("/todo", (req, res) => {
-  // const text = req.body;
-// using zod,
-  const response = todosSchema.safeParse(req.body);
+  const text = req.body;
+  // using zod,
+  // const response = todosSchema.safeParse(req.body);
 
-  if (!response.success) {
+  if (!text) {
     res.status(400).json({
-      masg: "Todo text is required.",
+      msg: "Todo text is required.",
     });
   }
 
   const newTodo = {
     id: Date.now(),
-    text: response.data.text,
+    text,
     completed: false,
   };
 
@@ -74,18 +87,17 @@ app.get("/todo/:id", (req, res) => {
 app.put("/todo/:id", (req, res) => {
   const todoId = req.params.id;
   if (!todoId) {
-    res.json({
+    res.status(400).json({
       msg: "Todo id is required",
     });
   }
   ALL_TODOS.forEach((singleTodo) => {
     if (singleTodo.id == todoId) {
-      singleTodo.completed = true
-        
+      singleTodo.completed = true;
     }
-    res.json({
-      singleTodo
-    })
+    res.status(200).json({
+      singleTodo,
+    });
   });
 });
 
@@ -96,11 +108,13 @@ app.delete("/todo/:id", (req, res) => {
     res.json({
       msg: "Todo id is required",
     });
-  };
+  }
   ALL_TODOS = ALL_TODOS.filter((singleTodo) => {
     return singleTodo.id !== todoId;
-  })
-})
+  });
+});
+
+app.use(errorHandlingMiddleware);
 
 app.listen(3000, () => {
   console.log("I am todo app server, running on port 3000");
